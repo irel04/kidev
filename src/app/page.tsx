7c873a0d74d9a1ea1ff798f6b387f0733d5b49e6
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-05-25 15:03:16 +0800

    chore: customize ui buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { ArrowUpRight } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="m-5">
       <h1 className="text-brand-500">Hello pipol</h1>
-      <Button>Click Me</Button>
+      <Button className="rounded-full" variant="primary" size="icon">
+        <ArrowUpRight/>
+      </Button>
     </div>
   );
 }
