2cc5009479870b0543e3b55ee7578fa207586cde
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-05-25 12:23:34 +0800

    chore: add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-5">
       <h1 className="text-brand-500">Hello pipol</h1>
-      <p>Home</p>
+      <Button>Click Me</Button>
     </div>
   );
 }
