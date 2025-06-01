7fc3426985f281750c533455741fe68455849edc
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-06-01 09:18:29 +0800

    style(button): adjust dark mode for outline variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-sm text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-sm text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[1px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive-500 text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border text-brand-500 shadow-xs border-brand-500 focus-visible:bg-brand-800 hover:bg-brand-500/50",
+          "border text-neutral-500 shadow-xs border-neutral-500 focus-visible:bg-neutral-100 hover:bg-neutral-100 dark:text-white dark:border-brand-500 dark:hover:bg-brand-500/50 dark:focus-visible:bg-brand-900",
         secondary:
           "bg-secondary-400 text-white shadow-xs hover:bg-secondary-600/90 focus-visible:ring-secondary-800",
         ghost:
